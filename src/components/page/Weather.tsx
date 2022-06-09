27d331bf90a1083a8bb2cab9fb6b5ec171c164da
watanabe0055 <mailok1212+github@gmail.com>
2022-06-10 07:19:16 +0900

    配列が１次元になっていたので２次元に修正した

--- src/components/page/Weather.tsx
+++ src/components/page/Weather.tsx
@@ -5,7 +5,7 @@ import { WeatherUrl } from "../weather/WeatherUrl";
 import { WeatherFetch } from "../weather/WeatherFetch";
 export const Weather = () => {
   const res = WeatherUrl();
-  console.log(res[0]);
+  console.log(res[1]);
 
   return <WeatherFetch url={"110000"} />;
 };
